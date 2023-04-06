e6c80cdc147388e376e90e45083b345a04b78962
Hughesy90 <alex.hughes90.ah@gmail.com>
2023-04-06 15:50:05 +1200

    Update About.jsx
    small updates

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,74 +1,10 @@
-// import React from 'react';
-
-// const About = () => {
-//   return (
-//     <div className="">
-//       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-//         <div className="lg:grid lg:grid-cols-2 lg:gap-8">
-//           <div>
-//             <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
-//               Our Story
-//             </h2>
-//             <p className="mt-3 max-w-3xl text-lg text-gray-500">
-//               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
-//               risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
-//               nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas
-//               ligula massa, varius a, semper congue, euismod non, mi.
-//             </p>
-//           </div>
-//           <div className="mt-8 lg:mt-0">
-//             <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
-//               Our Values
-//             </h2>
-//             <p className="mt-3 max-w-3xl text-lg text-gray-500">
-//               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
-//               risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
-//               nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas
-//               ligula massa, varius a, semper congue, euismod non, mi.
-//             </p>
-//           </div>
-//         </div>
-//         <div className="mt-16 lg:grid lg:grid-cols-2 lg:gap-8">
-//           <div>
-//             <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
-//               Our Team
-//             </h2>
-//             <p className="mt-3 max-w-3xl text-lg text-gray-500">
-//               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
-//               risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
-//               nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas
-//               ligula massa, varius a, semper congue, euismod non, mi.
-//             </p>
-//           </div>
-//           <div className="mt-8 lg:mt-0">
-//             <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
-//               Our Mission
-//             </h2>
-//             <p className="mt-3 max-w-3xl text-lg text-gray-500">
-//               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
-//               risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
-//               nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas
-//               ligula massa, varius a, semper congue, euismod non, mi.
-//             </p>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default About;
-
 import React from "react";
 
-
-
-
-function About() {
+const About = () => {
   return (
     <div className="bg-none">
       
-      <div className="max-w-7xl mx-auto py-20 px-4 sm:px-6 lg:px-8 py-12">
+      <div className="max-w-7xl mx-auto py-20 px-4 sm:px-6 lg:px-8">
         <div className="lg:text-center">
           <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
             About Us
@@ -113,7 +49,18 @@ function About() {
           </div>
         </div>
 
-        
+        <div className="max-w-7xl mx-auto py-20 px-4 sm:px-6 lg:px-8">
+        <div className="lg:text-center">
+          <p className=" text-white mt-2 text-3xl leading-8 font-extrabold tracking-tight  sm:text-4xl">
+            Charities
+          </p>
+          <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            magnam voluptatum cupiditate veritatis in, accusamus quisquam.
+          </p>
+        </div>
+
+        </div>
 
         </div>
       </div>
