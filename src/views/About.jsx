baaf10c445c22fdab891273d578d9ee839355205
Hughesy90 <alex.hughes90.ah@gmail.com>
2023-04-06 17:44:13 +1200

    About and Learn layout
    styling about and learn pages

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import bird from "../assets/images/bird.png"
 
 const About = () => {
   return (
@@ -6,60 +7,49 @@ const About = () => {
       
       <div className="max-w-7xl mx-auto py-20 px-4 sm:px-6 lg:px-8">
         <div className="lg:text-center">
-          <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+          <h2 className="text-base text-blue-600 font-semibold tracking-wide uppercase">
             About Us
           </h2>
           <p className=" text-white mt-2 text-3xl leading-8 font-extrabold tracking-tight  sm:text-4xl">
             The Universal Collective
           </p>
-          <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-            Non-fungalbe for Non-Profit
-          </p>
+        
+          <br />
+          <h1 className="  mt-4 max-w-2xl text-xl text-white lg:mx-auto">
+            Our Vision
+          </h1>
+          <p className="mt-4  text-white lg:mx-auto"> "The company's vision is to utilise a decentralized NFT marketplace that empowers creators to make a positive impact on society by supporting charities they care about. We believe that the power of art can be harnessed to make a difference in the world, and we want to enable artists to create while also giving back".</p>
+          <br />
+          <h1 className="  mt-4 max-w-2xl text-xl text-white lg:mx-auto">
+            Our Mission
+          </h1>
+          <p className="mt-4  text-white lg:mx-auto"> Our mission is to create a vibrant community of creators who are passionate about using their talents for good. We will provide a variety of tools and resources to help artists succeed, including marketing and promotional support, access to a community of fellow creators, and educational resources to help them improve their craft. Our goal is to make a positive impact on society, one NFT at a time.</p>
+
+
         </div>
 
-        <div className="mt-10">
-          <div className="md:grid md:grid-cols-2 md:gap-6">
-            <div>
-              <h3 className="text-lg font-medium leading-6 text-white">
-                Our Mission
-              </h3>
-              <div className="mt-2 text-gray-600">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Quo recusandae nulla rem eos ipsa praesentium esse magni
-                  similique, dolorum deleniti, officiis voluptatum corrupti
-                  consequuntur fuga voluptatibus? Rem, laudantium aliquid.
-                </p>
-              </div>
-            </div>
-            <div className="mt-10 md:mt-0">
-              <h3 className="text-lg font-medium leading-6 text-white">
-                Our Vision
-              </h3>
-              <div className="mt-2 text-gray-600">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Quo recusandae nulla rem eos ipsa praesentium esse magni
-                  similique, dolorum deleniti, officiis voluptatum corrupti
-                  consequuntur fuga voluptatibus? Rem, laudantium aliquid.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+
 
         <div className="max-w-7xl mx-auto py-20 px-4 sm:px-6 lg:px-8">
         <div className="lg:text-center">
           <p className=" text-white mt-2 text-3xl leading-8 font-extrabold tracking-tight  sm:text-4xl">
             Charities
           </p>
-          <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+          <p className="mt-4 max-w-2xl text-xl text-white lg:mx-auto">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
             magnam voluptatum cupiditate veritatis in, accusamus quisquam.
           </p>
         </div>
-
         </div>
+
+        <div className="flex justify-center items-center">
+      <div className="flex justify-between w-4/5">
+        <img src={bird} alt="Image 1" className="w-1/4" />
+        <img src={bird} alt="Image 2" className="w-1/4" />
+        <img src={bird} alt="Image 3" className="w-1/4" />
+        <img src={bird} alt="Image 4" className="w-1/4" />
+      </div>
+    </div>
 
         </div>
       </div>
--- src/views/NewToNFT.jsx
+++ src/views/NewToNFT.jsx
@@ -10,19 +10,19 @@ const NewToNFT = () => {
 
         {/* Section 1 */}
         <section className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+          <div className='pt-8 px-4 bg-[#9fccff] bg-opacity-10 backdrop-blur-3xl rounded-2xl shadow-lx shadow-black'>
           <div className="lg:text-center">
-          <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+          <h2 className="text-base text-blue-600 font-semibold tracking-wide uppercase">
            Learn
           </h2>
           
             <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">
               Non Fungable Tokens (NFTs)
             </p>
-            <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-              Lorem ipsum dolor sit amet consect adipiscing elit.Lorem ipsum dolor sit amet consect adipiscing elit.Lorem ipsum dolor sit amet consect adipiscing elit.Lorem ipsum dolor sit amet consect adipiscing elit.Lorem ipsum dolor sit amet consect adipiscing elit.
-            </p>
+            {/* <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto"> Lorem ipsum dolor sit amet consect adipiscing elit. Lorem ipsum dolor sit amet consect adipiscing elit.Lorem ipsum dolor sit amet consect adipiscing elit.Lorem ipsum dolor sit amet consect adipiscing elit.Lorem ipsum dolor sit amet consect adipiscing elit.Lorem ipsum dolor sit amet consect adipiscing elit.
+            </p> */}
           </div>
-          <div className="mt-10">
+          <div className="m-auto">
             <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
               <div className="col-span-1">
                 <div className="flex items-center justify-center h-96 text-white">
@@ -30,27 +30,27 @@ const NewToNFT = () => {
                 </div>
               </div>
               <div className="col-span-2">
-                <div className="flex items-center justify-center h-96 text-white ">
+                <div className="flex items-center justify-center h-96 pr-16 text-white ">
                  <p>An NFT is a unique digital asset that represents ownership of a piece of content, like artwork or music. They are stored on a blockchain, making ownership and transactions transparent and secure. NFTs allow creators to monetize their digital content and collectors to own unique pieces of digital art.</p>
                 </div>
               </div>
-             
             </div>
           </div>
+          </div>
         </section>
-        <hr className='my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-blue-500 to-transparent opacity-25 dark:opacity-100' />
+        {/* <hr className='my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-blue-500 to-transparent opacity-25 dark:opacity-100' /> */}
         {/* Section 2 */}
         <section className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+        <div className='pt-8 px-4 bg-[#9fccff] bg-opacity-10 backdrop-blur-3xl rounded-2xl shadow-lx shadow-black'>
           <div className="lg:text-center">
            
             <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">
               MetaMask (Wallet)
             </p>
-            <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-              Lorem ipsum dolor sit amet consect adipiscing elit. Lorem ipsum dolor sit amet consect adipiscing elit.Lorem ipsum dolor sit amet consect adipiscing elit.Lorem ipsum dolor sit amet consect adipiscing elit.Lorem ipsum dolor sit amet consect adipiscing elit.Lorem ipsum dolor sit amet consect adipiscing elit.
-            </p>
+            {/* <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto"> Lorem ipsum dolor sit amet consect adipiscing elit. Lorem ipsum dolor sit amet consect adipiscing elit.Lorem ipsum dolor sit amet consect adipiscing elit.Lorem ipsum dolor sit amet consect adipiscing elit.Lorem ipsum dolor sit amet consect adipiscing elit.Lorem ipsum dolor sit amet consect adipiscing elit.
+            </p> */}
           </div>
-          <div className="mt-10">
+          <div className="m-auto">
             <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
               <div className="col-span-1">
                 <div className="flex items-center justify-center h-96 text-white">
@@ -64,20 +64,22 @@ const NewToNFT = () => {
               </div>
             </div>
           </div>
+          </div>
         </section>
-        <hr className='my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-blue-500 to-transparent opacity-25 dark:opacity-100' />
+        {/* <hr className='my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-blue-500 to-transparent opacity-25 dark:opacity-100' /> */}
         {/* section 3 */}
         <section className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+        <div className='pt-8 px-4 bg-[#9fccff] bg-opacity-10 backdrop-blur-3xl rounded-2xl shadow-lx shadow-black'>
           <div className="lg:text-center">
           
             <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">
               Ethereum (Currency)
             </p>
-            <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            {/* <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
               Lorem ipsum dolor sit amet consect adipiscing elit. Lorem ipsum dolor sit amet consect adipiscing elit.Lorem ipsum dolor sit amet consect adipiscing elit.Lorem ipsum dolor sit amet consect adipiscing elit.Lorem ipsum dolor sit amet consect adipiscing elit.Lorem ipsum dolor sit amet consect adipiscing elit.
-            </p>
+            </p> */}
           </div>
-          <div className="mt-10">
+          <div className="m-auto">
             <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
               <div className="col-span-1">
                 <div className="flex items-center justify-center h-96 text-white">
@@ -90,6 +92,7 @@ const NewToNFT = () => {
                 </div>
               </div>
             </div>
+          </div>
           </div>
         </section>
      
