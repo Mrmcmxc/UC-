e3500e95e9db82d616a7e9a3a6e0ff4cfef2a515
Hughesy90 <alex.hughes90.ah@gmail.com>
2023-04-07 14:24:14 +1200

    Sticky Nav
    Working on show/hide function of the sticky nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,21 +15,6 @@ const Header = () => {
     <ul className="md:flex-[0.5] hidden md:flex list-none justify-end items-center flex-initial pr-4 ">
         <Link to="/" className="mx-4 cursor-pointer">Market</Link>
         <Link to="/Collections" className="mx-4 cursor-pointer ">Collections</Link>
-
-        {/* <button class="text-white font-medium rounded-lg px-4 py-2.5 text-center inline-flex items-center" type="button" data-dropdown-toggle="dropdown" >About <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
-
-        <div class=" bg-white text-base z-50 list-none divide-y divide-gray-100 rounded shadow my-4">
-          <div class="px-4 py-3"></div>
-          <ul class="py-1" aria-labelledby="dropdown">
-            <li>
-              <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">The Universal Collective</a>
-            </li>
-            <li>
-              <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Charities</a>
-            </li>
-          </ul>
-        </div> */}
- 
         <Link to="/About" className="mx-4 cursor-pointer">About</Link>
         <Link to="/NewToNFT" className="mx-4 cursor-pointer">Learn</Link>
     </ul>
@@ -40,3 +25,51 @@ const Header = () => {
 }
 
 export default Header
+
+
+//Sticky NAV
+// import React, {useEffect, useState} from "react";
+// import { Link } from "react-router-dom";
+
+// const Header = () => {
+//   const [show, setShow] = useState(true)
+//   const controlNavbar = () =>{
+//     if (window.scrollY>100) {
+//       setShow(false)
+//     }else{
+//       setShow(true)
+//     }
+//   }
+
+//   useEffect (() =>{
+//     window.addEventListener("scroll", controlNavbar)
+//     return () => {
+//       window.removeEventListener("scroll", controlNavbar)
+//     }
+//   }, [])
+
+//   return (
+//     <nav className={`nav ${show} bg-blue w-4/5 flex md:justify-center justify-between items-center py-12 mx-auto text-white sticky top-0 z-50`}>
+//       <div className="md:flex-[0.5] flex-initial justify-center items-center">
+//         {/* Logo/Name of site */}
+//         <Link to="/">
+//           <span className="mx-8 px-1 py-1 font-bold text-lg sm:text-2xl "> The Universal Collective</span>
+//         </Link>
+//       </div>
+//       {/* Navbar  */}
+//       <ul className="md:flex-[0.5] hidden md:flex list-none justify-end items-center flex-initial pr-4 ">
+//         <Link to="/" className="mx-4 cursor-pointer">Market</Link>
+//         <Link to="/Collections" className="mx-4 cursor-pointer ">Collections</Link>
+//         <Link to="/About" className="mx-4 cursor-pointer">About</Link>
+//         <Link to="/NewToNFT" className="mx-4 cursor-pointer">Learn</Link>
+//       </ul>
+//       <button className="shadow-md shadow-blue-700 bg-blue-500 hover:bg-blue-700 md:text-sm sm:text-base p-2 rounded-md">Connect Wallet</button>
+//     </nav>
+//   );
+// };
+
+// export default Header;
+
+
+
+
