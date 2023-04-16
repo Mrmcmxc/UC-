521b030f50e523cefcd8c66b9ac08a481e6c72bb
Mark Pepere <83042083+27011389@users.noreply.github.com>
2023-04-16 12:21:42 +1200

    Fixed Typos
    class => className
    Collections.jsx

--- src/views/Collections.jsx
+++ src/views/Collections.jsx
@@ -1,18 +1,20 @@
-import { useGlobalState } from '../store'
-import Artworks from '../components/Artworks'
-import Empty from '../components/Empty'
-import OfferItem from '../components/OfferItem'
+import { useGlobalState } from "../store";
+import Artworks from "../components/Artworks";
+import Empty from "../components/Empty";
+import OfferItem from "../components/OfferItem";
 
 const Collections = () => {
-  const [collections] = useGlobalState('collections')
+  const [collections] = useGlobalState("collections");
 
   return (
-    <div className='w-4/5 mx-auto'>
-      {collections.length > 0 ? (<Artworks  title=" Your collections" auctions={collections} showOffer />) : (<Empty />)}
-      <OfferItem />
-
+    <div className="w-4/5 mx-auto mt-11">
+      {collections.length > 0 ? (
+        <Artworks title="Your Collections" auctions={collections} showOffer />
+      ) : (
+        <Empty />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Collections
+export default Collections;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,29 +1,23 @@
-import Hero from "../components/Hero"
-import CreateNFT from "../components/CreateNFT"
-import Artworks from "../components/Artworks"
-import Empty from "../components/Empty"
-import { useGlobalState } from "../store"
-import HowITWorks from "../components/HowITWorks"
-import Wallets from "../components/Wallets"
-
-
-
+import Hero from "../components/Hero";
+import CreateNFT from "../components/CreateNFT";
+import Artworks from "../components/Artworks";
+import Empty from "../components/Empty";
+import { useGlobalState } from "../store";
+import HowITWorks from "../components/HowITWorks";
+import Wallets from "../components/Wallets";
 
 const Home = () => {
-  const [auctions] = useGlobalState('auctions')
+  const [auctions] = useGlobalState("auctions");
 
   return (
-    <div className='w-4/5 mx-auto'>
+    <div className="w-4/5 mx-auto">
       <Hero />
       <HowITWorks />
-      <CreateNFT/>
-      {/* <Artwork auctions={auctions}/> */}
-      {auctions.length > 0 ? <Artworks auctions={auctions} /> : <Empty/>}
+      {auctions.length > 0 ? <Artworks auctions={auctions} /> : <Empty />}
+      <CreateNFT />
       <Wallets />
-      
-      
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
